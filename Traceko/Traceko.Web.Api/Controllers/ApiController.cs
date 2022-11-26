3f55ff587e85f389fe184903e6aa0bd77246d471
Kirill Baranov <kirill.baranov@itentika.ru>
2022-11-26 14:40:22 +0300

    Minimal web api for prototyping

---
+++ Traceko/Traceko.Web.Api/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traceko.Web.Api.Controllers
+{
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/hello-async")]
+        public async Task<IActionResult> HelloAsync()
+        {
+            await Task.Delay(500);
+            return Ok($"Hello async. Timestamp {DateTime.Now}");
+        }
+
+        [HttpGet]
+        [Route("api/hello")]
+        public IActionResult Hello()
+        {
+            return Ok($"Hello. Timestamp {DateTime.Now}");
+        }
+    }
+}
---
+++ Traceko/Traceko.Web.Api/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Traceko.Web.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
